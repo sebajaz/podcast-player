1fbda5edbd251bd37ecc64d12dd23ec99a64d217
Sebastian Morinigo <sebajaz9@gmail.com>
2024-10-14 17:20:13 -0300

    ENTREGA NRO 4

--- src/main.jsx
+++ src/main.jsx
@@ -1,33 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { createContext, useContext, useEffect, useState, useRef } from "react";
 import Header from "./assets/components/header";
 import ListaPodcast from "./assets/components/listapodcast";
 import PlayBackBar from "./assets/components/playbackbar";
 
+const AudioContext = createContext();
+export const usarAudio = () => useContext(AudioContext);
+
+const AudioProvider = ({ children }) => {
+  const [sonando, setSonando] = useState(null);
+  const [reproduciendo, setReproduciendo] = useState(false);
+  const audioRef = useRef(null);
+
+  return (
+    <AudioContext.Provider value={{ sonando, setSonando, reproduciendo, setReproduciendo, audioRef }}>
+      {children}
+    </AudioContext.Provider>
+  );
+};
 
 const Main = () => {
   const [podcasts, setPodcasts] = useState([]);
-  const [suenaPodcast, setSuenaPodcast] = useState(null);
 
   useEffect(() => {
     fetch('https://api.audioboom.com/audio_clips')
-    .then((response) => response.json())
-    .then((data) => {
-      setPodcasts(data.body.audio_clips)
-    })
-    .catch((error) => console.error("ERROR EN FETCH", error))
-  }, []) 
-  const handlePodcastClick = (podcast) => {
-    setSuenaPodcast(podcast.urls.high_mp3)
-  }
-    return (
-      <div>
-        <Header/>
-        <div>
-          <ListaPodcast podcasts={podcasts} PodcastClick={handlePodcastClick}/>
-        </div>
-        {suenaPodcast && <PlayBackBar audioUrl={suenaPodcast}/>}
-      </div>
-    );
-  };
+      .then((response) => response.json())
+      .then((data) => {
+        setPodcasts(data.body.audio_clips);
+      })
+      .catch((error) => console.error("ERROR EN FETCH", error));
+  }, []);
+
+  return (
+    <AudioProvider>
+      <Header />
+      <ListaPodcast podcasts={podcasts} />
+      <PlayBackBar />
+    </AudioProvider>
+  );
+};
 
 export default Main;
